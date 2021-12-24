e3e195829f23ef7073589d6fb3adc5248fc9cfd7
Conor Ginty <conorginty@gmail.com>
2021-12-24 22:50:28 +0000

    Lec 26: Spring Pet Clinic - Initialising Spring PetClinic Application

---
+++ src/main/java/guru/springframework/sfgpetclinicconor/SfgPetClinicConorApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinicconor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicConorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgPetClinicConorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/sfgpetclinicconor/SfgPetClinicConorApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinicconor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicConorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
