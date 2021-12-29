996b2ffcfd3f7875003caca9475542c8e1c5756f
Conor Ginty <conorginty@gmail.com>
2021-12-29 23:03:48 +0000

    Lec 47: Spring Pet Clinic - POJO Data Model - Add POJOs. Closes #2

--- src/main/java/guru/springframework/sfgpetclinicconor/SfgPetClinicConorApplication.java
+++ src/main/java/guru/springframework/sfgpetclinicconor/SfgPetClinicConorApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SfgPetClinicConorApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SfgPetClinicConorApplication.class, args);
 	}
 
---
+++ src/main/java/guru/springframework/sfgpetclinicconor/model/Owner.java
@@ -0,0 +1,4 @@
+package guru.springframework.sfgpetclinicconor.model;
+
+public class Owner extends Person {
+}
