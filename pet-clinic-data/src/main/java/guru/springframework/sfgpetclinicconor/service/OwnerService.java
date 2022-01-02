5a06b2af4fa760d6cd7a9d1fe0d0a24a9dfcff91
Conor Ginty <conorginty@gmail.com>
2022-01-02 22:00:39 +0000

    Lec 61: Spring Pet Clinic - Refactor Services to Common CRUD Interface. Closes #10

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicconor/service/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicconor/service/OwnerService.java
@@ -2,11 +2,6 @@ package guru.springframework.sfgpetclinicconor.service;
 
 import guru.springframework.sfgpetclinicconor.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
-    Owner findById(Long id);
+public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName); // BUT SHOULDN'T THIS RETURN A SET OF OWNERS, AS THE LAST NAME COULD BE COMMON?
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicconor/service/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicconor/service/VetService.java
@@ -2,10 +2,6 @@ package guru.springframework.sfgpetclinicconor.service;
 
 import guru.springframework.sfgpetclinicconor.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet owner);
-    Set<Vet> findAll();
 }
