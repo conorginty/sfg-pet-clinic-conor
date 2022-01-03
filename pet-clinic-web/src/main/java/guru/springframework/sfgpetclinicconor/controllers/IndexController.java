b5f12d50da4bf4ea3e2ea3a9044ee53c02d06b34
Conor Ginty <conorginty@gmail.com>
2022-01-03 10:34:07 +0000

    Lec 64: Spring Pet Clinic - Create Index Page and Controller. Closes #7

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinicconor/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgpetclinicconor.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    public static final String INDEX = "index";
+
+    @RequestMapping({"", "/", INDEX, INDEX+".html"})
+    public String index() {
+        return "index";
+    }
+}
