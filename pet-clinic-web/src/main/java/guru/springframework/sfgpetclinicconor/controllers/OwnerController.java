b4a5f7309c2c29dc8421cb9289f5d5f31e9bb663
Conor Ginty <conorginty@gmail.com>
2022-01-03 14:15:03 +0000

    Lec 67: Spring Pet Clinic - Create Owner Index Page and Controller. Closes #12

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinicconor/controllers/OwnerController.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinicconor.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+    @RequestMapping({"", "/", "/index"})
+    public String listOwners() {
+        return "owners/index";
+    }
+}
