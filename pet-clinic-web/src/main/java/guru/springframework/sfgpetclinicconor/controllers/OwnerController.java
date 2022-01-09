cc5a772f904ccd8b9215824c4b959b6204d67d6c
Conor Ginty <conorginty@gmail.com>
2022-01-09 23:29:40 +0000

    Lec 82: Spring Pet Clinic - List Owners on the Owners Index Page. Closes #15

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinicconor/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinicconor/controllers/OwnerController.java
@@ -1,13 +1,25 @@
 package guru.springframework.sfgpetclinicconor.controllers;
 
+import guru.springframework.sfgpetclinicconor.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 }
