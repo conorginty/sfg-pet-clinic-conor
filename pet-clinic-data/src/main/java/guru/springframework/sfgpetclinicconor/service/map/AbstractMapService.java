72d34e5802b3c1e974afc1945dbf7e0433203b65
Conor Ginty <conorginty@gmail.com>
2022-01-10 23:11:33 +0000

    Lec 84: Spring Pet - Auto-Generate Map IDs. Closes #22

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicconor/service/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicconor/service/map/AbstractMapService.java
@@ -1,12 +1,11 @@
 package guru.springframework.sfgpetclinicconor.service.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import guru.springframework.sfgpetclinicconor.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
-    protected Map<ID, T> map = new HashMap<>();
+import java.util.*;
+
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -16,8 +15,27 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        map.putIfAbsent(id, object);
+    T save(T object) {
+        // Nested if Impl (bad - harder to read)
+//        if (object != null) {
+//            if (object.getId() == null) {
+//                object.setId(getNextId());
+//            }
+//            map.put(object.getId(), object);
+//        } else {
+//            throw new RuntimeException("Object cannot be null");
+//        }
+
+        // Non-nested Control Flow (better: guard clause - easier to read)
+        if (object == null) {
+            throw new RuntimeException("Object cannot be null");
+        }
+
+        if (object.getId() == null) {
+            object.setId(getNextId());
+        }
+        map.put(object.getId(), object);
+
         return object;
     }
 
@@ -27,5 +45,23 @@ public abstract class AbstractMapService<T, ID> {
 
     void delete(T object) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId() {
+        // Tutorial Impl (BAD) - Uses Exception Handling for Control Flow (anti-pattern and code smell: https://softwareengineering.stackexchange.com/questions/189222/are-exceptions-as-control-flow-considered-a-serious-antipattern-if-so-why)
+//        Long nextId;
+//        try {
+//            nextId = Collections.max(map.keySet()) + 1;
+//        } catch (NoSuchElementException e) {
+//            nextId = 1L;
+//        }
+//        return nextId;
+
+        // Impl from Comment Section (better) - uses Conditional Guard Logic for Control Flow
+        if (this.map.isEmpty()) {
+            return 1L;
+        } else {
+            return (long) (map.size() + 1);
+        }
     }
 }
