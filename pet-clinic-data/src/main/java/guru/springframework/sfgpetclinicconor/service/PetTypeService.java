7891370cbf98fd4f839d3f7802559f14177e57f3
Conor Ginty <conorginty@gmail.com>
2022-01-26 23:03:43 +0000

    Lec 122: Spring Pet Clinic - Create PetType Map Service Implementation. Closes #27

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicconor/service/PetTypeService.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinicconor.service;
+
+import guru.springframework.sfgpetclinicconor.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicconor/service/map/PetTypeServiceMap.java
@@ -0,0 +1,36 @@
+package guru.springframework.sfgpetclinicconor.service.map;
+
+import guru.springframework.sfgpetclinicconor.model.PetType;
+import guru.springframework.sfgpetclinicconor.service.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+}
