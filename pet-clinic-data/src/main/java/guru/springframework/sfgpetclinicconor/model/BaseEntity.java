fbeaefa25fed65640ca97aea667ed6213ec3171e
Conor Ginty <conorginty@gmail.com>
2022-02-20 20:49:19 +0000

    Lec 146 - Spring Pet Clinic - Create Base Entity. Closes #33

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicconor/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinicconor/model/BaseEntity.java
@@ -1,8 +1,15 @@
 package guru.springframework.sfgpetclinicconor.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass // Establishes this class as a base class to JPA, and we don't need this mapped to the DB
 public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
